d07b4df496ec8f8f6c12f1b4c88473e4278b7c8e
Richard Seward <raseward14@gmail.com>
2021-04-01 17:59:05 -0600

    GET routes complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -25,6 +25,26 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id,
+      {
+        // include products within category
+        include: [
+        { model: Category },
+        { model: Tag, through: ProductTag, as: 'product_tags' }
+        ]
+      });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // create new product
